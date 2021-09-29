c073433808c8aa477ab538e31baca101753d4e1b
Yesid Varela <yleandrovarela@gmail.com>
2021-09-29 14:13:55 -0500

    Funcion para subir foto del avatar

--- src/components/User/AvatarForm/AvatarForm.js
+++ src/components/User/AvatarForm/AvatarForm.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Button } from "semantic-ui-react";
+import { useDropzone } from "react-dropzone";
 import "./AvatarForm.scss";
 
 export default function AvatarForm(props) {
   const { setShowModal } = props;
+
+  const onDrop = useCallback((acceptedFile) => {
+    console.log(acceptedFile);
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: "image/jpeg, image/png",
+    noKeyboard: true,
+    multiple: false,
+    onDrop,
+  });
   return (
     <div className="avatar-form">
-      <Button>Cargar una foto</Button>
+      <Button {...getRootProps()}>Cargar una foto</Button>
       <Button>Eliminar foto actual</Button>
       <Button onClick={() => setShowModal(false)}>Cancelar</Button>
+      <input {...getInputProps()} />
     </div>
   );
 }
